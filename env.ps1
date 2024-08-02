ec38af9222f2f5102f4d73147d433cd939e0f24f
nickarino <nick@ourdecisions.com>
2024-08-01 22:22:10 -0400

    Stop tracking env.ps1

--- env.ps1
+++
@@ -1,5 +0,0 @@
-# env.ps1 
-# In your script or profile, dot-source the env.ps1 file to load the environment variables:
-# . ./env.ps1
-$env:MySecretKey = 'your-secret-key'
-$env:DatabasePassword = 'your-database-password'
